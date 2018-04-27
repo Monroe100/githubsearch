c5bd7cd2d702e337d9fa34a26602cc7946875aed
Monroe100 <michelle1hunnid@gmail.com>
2018-04-27 15:31:54 +0300

    modified app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { GitProfileComponent } from './git-profile/git-profile.component';
@@ -12,7 +12,8 @@ import { GitProfileComponent } from './git-profile/git-profile.component';
     GitProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/git-profile/git-profile.component.ts
+++ src/app/git-profile/git-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-git-profile',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./git-profile.component.css']
 })
 export class GitProfileComponent implements OnInit {
+// http:HttpClient
 
-  constructor() { }
+repos=[];
+
+link = 'https://api.github.com/users/Monroe100/repos?access_token=1526a37ff44f83ff7c9591722772a9b3903d6f01';
+
+  constructor(private http:HttpClient) { 
+    this.http = http
+  }
 
   ngOnInit() {
+    this.http.get(this.link).subscribe((res:any)=>{
+      this.repos=res
+      console.log("Data: " +this.repos);
+      
+    });
   }
 
 }
